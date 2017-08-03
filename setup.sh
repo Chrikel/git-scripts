57c2dfe385036d737c4f91bf774399910da906d6
Christopher Krause <christopher.krause@magicline.de>
2017-08-03 06:55:08 +0200

    feat: Edit setup file to use shell config file

--- setup.sh
+++ setup.sh
@@ -2,11 +2,29 @@
 
 CURRENTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCRIPTDIR="$CURRENTDIR/scripts/"
+SHELLCONFIG=""
+
+if [ -r ~/.bashrc ] 
+then 
+    SHELLCONFIG="~/.bashrc"
+fi
+
+if [ -r ~/.zshrc ]
+then
+    SHELLCONFIG="~/.zshrc"
+fi
 
 if [[ $SCRIPTDIR != *"$PATH"*  ]]
 then 
-    echo "export PATH=\$PATH:$SCRIPTDIR" >> ~/.bashrc
-    . ~/.bashrc    
+   if [ -z $SHELLCONFIG ]
+   then
+        echo "No shellconfig file found"
+        echo "Add line to your shell config"
+        echo "export PATH=\$PATH:$SCRIPTDIR"
+    else
+        echo "export PATH=\$PATH:$SCRIPTDIR" >> $SHELLCONFIG
+        . $SHELLCONFIG
+    fi    
 else
     echo "Setup already done"
 fi
